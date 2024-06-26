def1b2991031ed73aaadb2311f42d67f214513e8
eduzissimo <eduardo.alves14@hotmail.com>
2024-06-25 21:24:40 -0300

    Initial commit: test_ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
+
+INGREDIENT_RESTRICTION = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("queijo mussarela")
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions == INGREDIENT_RESTRICTION
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
+    assert hash(ingredient) == hash("queijo mussarela")
+
+    ingredient2 = Ingredient("farinha")
+    ingredient3 = Ingredient("farinha")
+    ingredient4 = Ingredient("bacon")
+    assert ingredient2 == ingredient3
+    assert ingredient2 != ingredient4
